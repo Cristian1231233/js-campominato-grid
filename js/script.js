525c1563031110a9eb5936b80275f1906308863c
Cristian1231233 <campagnacristian22@gmail.com>
2021-11-02 16:07:20 +0100

    commit container

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('ciao');
+
+const container = document.querySelector('.countain-wrap');
+console.log(container);
+const play = document.querySelector('.play');
+console.log(play);
+
+
+// creo un ciclo for in cui inserisco gli square
+for( let i = 1; i < 64; i++){
+    const sq = document.createElement('div');
+    sq.className = 'square';
+    container.append(sq);
+    
+}
