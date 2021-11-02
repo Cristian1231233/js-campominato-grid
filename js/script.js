87b2434dad1fcccb3df530bcb2f879f461889c2c
Cristian1231233 <campagnacristian22@gmail.com>
2021-11-02 17:04:27 +0100

    altro commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,32 @@
 
-const container = document.querySelector('.countain-wrap');
+let container = document.querySelector('.countain-wrap');
 console.log(container);
 const play = document.querySelector('.play');
 console.log(play);
+const testo = document.querySelector('.testo');
+
+
 
 
 // creo un ciclo for in cui inserisco gli square
-for( let i = 1; i < 64; i++){
+for( let i = 1; i < 101; i++){
     const sq = document.createElement('div');
     sq.className = 'square';
+    sq.innerHTML = `<span> ${numerRandom(1, 100)}</span>`;
     container.append(sq);
-    
+    sq.addEventListener('click', function(){
+        this.classList.add('clicked');
+        sq.className('none');
+    });
+}
+
+
+
+
+
+
+
+
+function numerRandom(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
